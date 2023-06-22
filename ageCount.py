aa6bdbab9aea9ead8d714ce481d17a00f9d9e838
Uğur Başdaş <99135103+basdasugur@users.noreply.github.com>
2023-06-22 17:37:02 +0300

    Create ageCount.py

---
+++ ageCount.py
@@ -0,0 +1,14 @@
+age = int(input("What is your age ? : "))
+score = 90
+
+year = 365
+week = 52
+month = 12
+
+result = 90 - age
+
+days = result * year
+weeks = result * week
+months = result * month
+
+print(f"If you will live until 90 years, you have {result} years, \n{months} months, {weeks} week and {days} days left. \nDon't worry, Please be happy :)")
